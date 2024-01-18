9008d40a52c86996c4e8244eea455f94f4f9761d
Muhammad Shahbaz Rafique <97734247+Shahbaz-Rafique@users.noreply.github.com>
2024-01-18 07:45:59 -0800

    commit

--- utils/multer.js
+++ utils/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 var storage=multer.diskStorage({
     destination:function(req,file,cb){
-        cb(null,"./public/files/");
+        cb(null,"./public/images/");
     },
     filename:function(req,file,cb){
         cb(null,Date.now()+file.originalname)
